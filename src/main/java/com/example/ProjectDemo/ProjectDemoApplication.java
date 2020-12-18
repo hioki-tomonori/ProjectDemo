765ba6e4892d84e83a1db4bf133ffbfa9ef62308
hioki-tomonori <76200438+hioki-tomonori@users.noreply.github.com>
2020-12-18 19:21:14 +0900

    change

--- src/main/java/com/example/ProjectDemo/ProjectDemoApplication.java
+++ src/main/java/com/example/ProjectDemo/ProjectDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectDemoApplication {
 
-	// comment 1
+	// comment 2
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectDemoApplication.class, args);
 	}
