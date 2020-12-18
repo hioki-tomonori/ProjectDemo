a4c7245eb384ed69ae39405976fb37a47e84d524
日置 智紀 <hioki@sec.co.jp>
2020-12-18 19:14:47 +0900

    change ProjectDemoApplication

--- src/main/java/com/example/ProjectDemo/ProjectDemoApplication.java
+++ src/main/java/com/example/ProjectDemo/ProjectDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectDemoApplication {
 
+	// comment 1
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectDemoApplication.class, args);
 	}
